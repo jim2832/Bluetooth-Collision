684b8cdf23958cf5cafdf758742b18f2fff90a44
jim2832 <jim880705@gmail.com>
2022-12-11 06:55:11 +0800

    221210 update

--- problem1.cpp
+++ problem1.cpp
@@ -6,8 +6,11 @@
 #include <unistd.h>
 using namespace std;
 
+#define SIM_CNT 200
+#define HOPPING_FREQENCY 1600
 #define MAX_DEV_NUM 100
 #define CHANNEL 79
+
 
 void clean_array(int *arr){
     int len = sizeof(arr)/sizeof(int);
@@ -20,18 +23,21 @@ void clean_array(int *arr){
 int main(){
     //int *device = new int[MAX_DEV_NUM]; //裝置
     int device_num[] = {2}; //裝置的數量
-    int *channel_seq = new int[MAX_DEV_NUM]; //儲存碰撞的陣列
+    int *channel_seq = new int[MAX_DEV_NUM]; //儲存測試的陣列
+    int *collision_seq = new int[MAX_DEV_NUM]; //儲存碰撞的陣列
+    int *total_time = new int[MAX_DEV_NUM]; //總次數
+    double *Prob = new double[MAX_DEV_NUM]; //碰撞機率
     int collision_cnt = 0; //碰撞次數
     srand(time(NULL)); //隨機seed
 
-    for(int i=0; i<sizeof(device_num)/sizeof(device_num[0]); i++){
+    for(int i=0; i<sizeof(device_num)/sizeof(device_num[0]); i++){ //目前sizeof(device_num)/sizeof(device_num[0]) = 1
         int *device = new int[device_num[i]]; //儲存裝置的陣列
         //cout << sizeof(device)/sizeof(device[0]) << endl; //print 2 (當前模擬裝置的數量)
 
         //模擬200次
-        for(int sim_cnt=0; sim_cnt<200; sim_cnt++){
+        for(int sim_cnt=0; sim_cnt<SIM_CNT; sim_cnt++){
             //每個模擬跳頻1600次
-            for(int freq=0; freq<1600; freq++){
+            for(int freq=0; freq<HOPPING_FREQENCY; freq++){
                 //各個裝置
                 for(int dev=0; dev<sizeof(device)/sizeof(device[0]); dev++){
                     int rand_num = rand() % (CHANNEL + 1);
@@ -41,24 +47,30 @@ int main(){
                         cout << channel_seq[dev-1] << "\t" << channel_seq[dev];
                         if(channel_seq[dev-1] == channel_seq[dev]){
                             collision_cnt++;
-                            cout << "\t<- Collision!" << endl;
+                            //cout << "\t<- Collision!" << endl;
                         }
-                        else{
-                            cout << endl;
-                        }
+                        // else{
+                        //     cout << endl;
+                        // }
                         clean_array(channel_seq);
                     }
                 }
             }
         }
-        cout << endl << "有2個裝置的碰撞次數為: " << collision_cnt << endl;
-        sleep(1);
-        
+        //cout << endl << "有2個裝置的碰撞次數為: " << collision_cnt << endl;
+
+        collision_seq[i] = collision_cnt;
+        total_time[i] = SIM_CNT * HOPPING_FREQENCY * device_num[i];
+        Prob[i] = (double)collision_seq[i] / total_time[i];
+        cout << total_time[i] << endl;
+        cout << "有 " << device_num[i] << " 個裝置的碰撞機率為: "  << Prob[i] << endl;
         
         delete[] device;
     }
 
     delete[] channel_seq;
+    delete[] collision_seq;
+    delete[] Prob;
 
     return 0;
 }
